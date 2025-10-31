3af9d886f92122eeb83d5a3bbf8e6cf6723ac065
Bibek <npbibek94@gmail.com>
2025-10-31 13:37:55 +0545

    simple program

---
+++ lib/area_of_circle.dart
@@ -0,0 +1,10 @@
+//naming onvention 
+//pascalCase:class
+// CamelCase=varibales,function 
+//snake_case=file name 
+void main(){
+  //area of cirle if  pie is inserted by user use that other wise use 3.14
+
+
+}
+
